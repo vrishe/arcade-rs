447d30b1e352d9c91dd332757347f082a5b91af2
Vladislav Rishe <vlados.v10.0@gmail.com>
2016-06-23 10:59:43 +0300

    Step 8: Parallax background is added.

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -3,6 +3,7 @@ use phi::data::Rectangle;
 use phi::gfx::Sprite;
 
 use sdl2::pixels::Color;
+use sdl2::render::Renderer;
 
 
 // Constants
@@ -10,6 +11,13 @@ const PLAYER_SPEED: f64 = 180.0;
 
 const SHIP_W: f64 = 64.0;
 const SHIP_H: f64 = 64.0;
+
+
+#[cfg(feature = "debugging")]
+const DEBUG: bool = true;
+#[cfg(not(feature = "debugging"))]
+const DEBUG: bool= false;
+
 
 /// The different states our ship might be in. In the image, they're ordered
 /// from left to right, then from top to bottom.
@@ -34,8 +42,53 @@ struct Ship {
     current: ShipFrame,
 }
 
+#[derive(Clone)]
+struct Background {
+    pos: f64,
+    // The amount of pixels moved to the left every second
+    vel: f64,
+    sprite: Sprite,
+}
+
+impl Background {
+    fn render(&mut self, renderer: &mut Renderer, elapsed: f64) {
+        // We define a logical position as depending solely on the time and the
+        // dimensions of the image, not on the screen's size.
+        let size = self.sprite.size();
+        self.pos += self.vel * elapsed;
+        if self.pos > size.0 {
+            self.pos -= size.0;
+        }
+
+        // We determine the scale ratio of the window to the sprite.
+        let (win_w, win_h) = renderer.output_size().unwrap();
+        let scale = win_h as f64 / size.1;
+
+        // We render as many copies of the background as necessary to fill
+        // the screen.
+        let mut physical_left = -self.pos * scale;
+
+        while physical_left < win_w as f64 {
+            //? While the left of the image is still inside of the window...
+            self.sprite.render(renderer, Rectangle {
+                x: physical_left,
+                y: 0.0,
+                w: size.0 * scale,
+                h: win_h as f64,
+            });
+
+            physical_left += size.0 * scale;
+        }
+    }
+}
+
+
 pub struct ShipView {
     player: Ship,
+
+    bg_back: Background,
+    bg_middle: Background,
+    bg_front: Background,
 }
 
 impl ShipView {
@@ -70,6 +123,22 @@ impl ShipView {
                 },
                 sprites: sprites,
                 current: ShipFrame::MidNorm,
+            },
+
+            bg_back: Background {
+                pos: 0.0,
+                vel: 20.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starBG.png").unwrap(),
+            },
+            bg_middle: Background {
+                pos: 0.0,
+                vel: 40.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starMG.png").unwrap(),
+            },
+            bg_front: Background {
+                pos: 0.0,
+                vel: 80.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starFG.png").unwrap(),
             },
         }
     }
@@ -122,6 +191,15 @@ impl View for ShipView {
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
 
+        // Render Backgrounds
+        self.bg_back.render(&mut phi.renderer, elapsed);
+        self.bg_middle.render(&mut phi.renderer, elapsed);
+
+        // Render the bounding box (for debugging purposes)
+        if DEBUG {
+            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+            phi.renderer.fill_rect(self.player.rect.to_sdl().unwrap());
+        }
         // Select the appropriate sprite of the ship to show.
         self.player.current =
         if dx == 0.0 && dy < 0.0       { ShipFrame::UpNorm }
@@ -137,6 +215,9 @@ impl View for ShipView {
 
         self.player.sprites[self.player.current as usize]
         .render(&mut phi.renderer, self.player.rect);
+
+        // Render the foreground
+        self.bg_front.render(&mut phi.renderer, elapsed);
 
         ViewAction::None
     }
